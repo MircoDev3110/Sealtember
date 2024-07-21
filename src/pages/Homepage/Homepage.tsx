7370ebe7533402184728ac3ed3c59078f411ccc9
MircoDev3110 <mirco.wrk@gmail.com>
2024-07-21 15:47:30 +0200

    fix: updated texts

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -37,7 +37,7 @@ const Homepage : React.FC = () => {
                 <div className="home-bighero-container">
                     <div className="bighero-text">
                         <h1 className="bighero-title">È tornato il Sealtember!</h1>
-                        <h3 className="bighero-subtitle">30 giorni, 30 foche e 30 movimenti artistici diversi per dare spazio alla creatività! Scorri per scoprire l'elenco dei prompt e per trovare l'ispirazione con le schede dedicate a ciascun movimento artistico</h3>
+                        <h3 className="bighero-subtitle">30 giorni, 30 foche e 30 movimenti artistici diversi per dare spazio alla creatività! Scorri per scoprire l'elenco dei temi e per trovare l'ispirazione con le schede dedicate a ciascun movimento artistico</h3>
                         <button onClick={() => scrollToPrompts()} className="default-btn">Vai ai Prompt</button>
                     </div>
                     <div className="bighero-image" style={{ backgroundImage: `url("${bigheroImage}")` }}></div>
@@ -53,7 +53,7 @@ const Homepage : React.FC = () => {
                         Le regole del Sealtember sono semplici: ogni giorno del mese è associato ad una tematica da cui lasciarsi ispirare, è possibile disegnare tutti i temi o solo alcuni; qualsiasi forma d'arte è accettata e valida, dal tradizionale al digitale, dal 3D alla scultura.
                         La regola principale, nonché la più importante, è la seguente: ogni disegno del Sealtember deve avere come soggetto una (o più) foche!
                         <br/><br/>
-                        Esplorate l'elenco dei prompt per questa edizione del Sealtember che trovate qui sotto e date sfogo alla fantasia, se prendete parte alla challenge e volete pubblicare i vostri disegni sui profili social ricordatevi di usare l'hashtag #Sealtember2024!
+                        Esplorate l'elenco dei temi per questa edizione del Sealtember che trovate qui sotto e date sfogo alla fantasia, se prendete parte alla challenge e volete pubblicare i vostri disegni sui profili social ricordatevi di usare l'hashtag #Sealtember2024!
                     </p>
                 </div>
             </section>
@@ -62,8 +62,8 @@ const Homepage : React.FC = () => {
                 <div className='prompt-col'>
 
                     <div className='prompt-title-wrapper'>
-                        <h2 className='prompt-title'>Elenco dei Prompt</h2>
-                        <p className='prompt-subtitle'>Qui sono elencati i 30 prompt per questa edizione del sealtember, cliccando su ciascun prompt potrai vedere una scheda dedicata al movimento artistico per scoprirne di più e trovare ispirazione</p>
+                        <h2 className='prompt-title'>Elenco dei Temi</h2>
+                        <p className='prompt-subtitle'>Qui sono elencati i 30 movimentiu artistici per questa edizione del Sealtember, cliccando su ciascun tema potrai vedere una scheda dedicata al movimento artistico per scoprirne di più e trovare ispirazione</p>
                     </div>
 
                     <div className='prompt-list-container'>
